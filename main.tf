c6e2ae16183b14af23cd55ae75405f7f0b7841cf
starseed777 <waress66@live.com>
2022-11-26 14:41:00 -0500

    tf backend added

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "orchsky-storage"
+    storage_account_name = "orchskystorage"
+    container_name       = "orchsky-tf"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
